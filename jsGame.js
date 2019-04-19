8f7dee6018d93a362aedca00f4efc237ad79714a
MartykQ <fmartyka16@gmail.com>
2019-04-19 10:36:28 +0200

    dom creation func

--- jsGame.js
+++ jsGame.js
@@ -118,6 +118,19 @@ class Coin {
 Coin.prototype.size = new Vec(0.6, 0.6);
 
 
+
+function elt(name, attrs, ...children) {
+    let dom = document.createElement(name);
+    for(let attr of Object.keys(attrs)) {
+        dom.setAttribute(attr, attrs[attr]);
+
+    }
+    for(let child of children) {
+        dom.appendChild(child);
+    }
+    return dom;
+}
+
 const levelChars = {
     "." : "empty",
     "#": "wall",
